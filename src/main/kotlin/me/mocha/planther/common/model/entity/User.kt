14b2ed0c99768ff28cf991daa89f26850787d110
hwangseonu <hwangseonu12@naver.com>
2019-01-30 00:26:00 +0900

    feat: Add user model

--- src/main/kotlin/me/mocha/planther/common/model/entity/User.kt
+++ src/main/kotlin/me/mocha/planther/common/model/entity/User.kt
@@ -1,13 +1,24 @@
 package me.mocha.planther.common.model.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
 data class User(
-    @Id val username: String,
-    val password: String,
-    val name: String,
-    val grade: Int,
-    val cls: Int,
-    val number: Int)
+        @Id val username: String,
+        @JsonIgnore val password: String,
+        val name: String,
+        val grade: Int,
+        val cls: Int,
+        val number: Int,
+        @JsonIgnore val roles: ArrayList<String>) {
+
+    public fun getStudentId(): String {
+        return String.format("%d%02d%02d", grade, cls, number)
+    }
+
+    public fun getClassId(): String {
+        return String.format("%d%02d", grade, cls)
+    }
+}
--- src/main/kotlin/me/mocha/planther/common/model/repository/UserRepository.kt
+++ src/main/kotlin/me/mocha/planther/common/model/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package me.mocha.planther.common.model.repository
 import me.mocha.planther.common.model.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, String>
+interface UserRepository : JpaRepository<User, String> {
+    fun existsByGradeAndClsAndNumber(grade: Int, cls: Int, number: Int): Boolean
+}
