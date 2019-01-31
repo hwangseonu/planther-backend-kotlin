e50bc7313bb9ac48bcdbf411fa047fa833ff88b1
hwangseonu <hwangseonu12@naver.com>
2019-01-31 11:59:09 +0900

    feat: Add user model

--- src/main/kotlin/me/mocha/planther/common/model/entity/User.kt
+++ src/main/kotlin/me/mocha/planther/common/model/entity/User.kt
@@ -1,18 +1,35 @@
 package me.mocha.planther.common.model.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
 data class User(
-        @Id val username: String,
-        @JsonIgnore val password: String,
+        @Id
+        @Column(nullable = false)
+        val username: String,
+
+        @Column(nullable = false)
+        @JsonIgnore
+        val password: String,
+
+        @Column(nullable = false)
         val name: String,
+
+        @Column(nullable = false)
         val grade: Int,
+
+        @Column(nullable = false)
         val cls: Int,
+
+        @Column(nullable = false)
         val number: Int,
-        @JsonIgnore val roles: ArrayList<String>) {
+
+        @Column(nullable = false)
+        @JsonIgnore
+        val roles: ArrayList<String>) {
 
     public fun getStudentId(): String {
         return String.format("%d%02d%02d", grade, cls, number)
